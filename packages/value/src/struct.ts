4aa57155065cfbf9fb4e84462c351cf448ced973
Vladislav Polyakov <polrk@ydb.tech>
2025-04-02 17:32:22 +0000

    Enhance StructType and Struct classes with sorting functionality and optional handling

--- packages/value/src/struct.ts
+++ packages/value/src/struct.ts
@@ -1,17 +1,30 @@
 import { create } from '@bufbuild/protobuf';
-import * as Ydb from '@ydbjs/api/value'
+import * as Ydb from '@ydbjs/api/value';
 
+import { Optional } from './optional.js';
 import { TypeKind, type Type } from "./type.js";
 import type { Value } from './value.js';
 
 export class StructType implements Type {
-	readonly names: string[];
-	readonly types: Type[];
+	readonly names: string[] = [];
+	readonly types: Type[] = [];
 	#typeInstance?: Ydb.Type;
 
-	constructor(names: string[], types: Type[]) {
-		this.names = names;
-		this.types = types;
+	constructor(names: string[], types: Type[], sorted = false) {
+		if (sorted) {
+			this.names = names;
+			this.types = types;
+
+			return
+		}
+
+		const indices = names.map((_, i) => i);
+		indices.sort((a, b) => names[a].localeCompare(names[b]));
+
+		for (let i of indices) {
+			this.names.push(names[i]);
+			this.types.push(types[i]);
+		}
 	}
 
 	get kind(): TypeKind.STRUCT {
@@ -30,6 +43,12 @@ export class StructType implements Type {
 
 		return this.#typeInstance;
 	}
+
+	*[Symbol.iterator](): Iterator<[string, Type]> {
+		for (let i = 0; i < this.names.length; i++) {
+			yield [this.names[i], this.types[i]];
+		}
+	}
 }
 
 export class Struct<T extends Record<string, Value> = Record<string, Value>> implements Value<StructType> {
@@ -37,17 +56,37 @@ export class Struct<T extends Record<string, Value> = Record<string, Value>> imp
 	readonly items: Value[] = [];
 	#valueInstance?: Ydb.Value;
 
-	constructor(value: T) {
+	constructor(obj: T, def?: StructType) {
+		if (def) {
+			this.type = def;
+
+			for (let [name, type] of def) {
+				let value = obj[name]
+				if (value && value.type.kind !== type.kind) {
+					throw new Error(`Invalid type for ${name}: expected ${type.kind}, got ${value.type.kind}`);
+				}
+
+				this.items.push(value ? value : new Optional(null, type));
+			}
+
+			return
+		}
+
+		let keys = Object.keys(obj)
 		let names: string[] = [];
 		let types: Type[] = [];
 
-		for (let key in value) {
-			names.push(key);
-			types.push(value[key].type);
-			this.items.push(value[key]);
+		// Sort both arrays based on names
+		const indices = keys.map((_, i) => i);
+		indices.sort((a, b) => keys[a].localeCompare(keys[b]));
+
+		for (let i of indices) {
+			names.push(keys[i]);
+			types.push(obj[keys[i]].type);
+			this.items.push(obj[keys[i]]);
 		}
 
-		this.type = new StructType(names, types);
+		this.type = new StructType(names, types, true);
 	}
 
 	encode(): Ydb.Value {
