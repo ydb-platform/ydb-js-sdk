1c9fee0383fa66b3bca23870333f176fed0997c1
Vladislav Polyakov <polrk@ydb.tech>
2025-04-03 18:06:42 +0000

    Implement retry logic in discovery method to enhance error handling and ensure successful endpoint retrieval

--- packages/core/src/driver.ts
+++ packages/core/src/driver.ts
@@ -7,8 +7,10 @@ import { DiscoveryServiceDefinition, EndpointInfoSchema, ListEndpointsResultSche
 import { StatusIds_StatusCode } from '@ydbjs/api/operation';
 import type { CredentialsProvider } from '@ydbjs/auth';
 import { AnonymousCredentialsProvider } from '@ydbjs/auth/anonymous';
+import { YDBError } from '@ydbjs/errors';
+import { retry, type RetryConfig } from '@ydbjs/retry';
+import { exponential } from '@ydbjs/retry/strategy';
 import { ClientError, composeClientMiddleware, Metadata, Status, waitForChannelReady, type ChannelOptions, type Client, type ClientMiddleware, type CompatServiceDefinition } from 'nice-grpc';
-
 import { LazyConnection, type Connection, type ConnectionCallOptions } from './conn.ts';
 import { dbg } from './dbg.js';
 import { ConnectionPool } from './pool.js';
@@ -28,9 +30,9 @@ export type DriverOptions = ChannelOptions & {
 
 const defaultOptions: DriverOptions = {
 	'ydb.sdk.enable_discovery': true,
-	'ydb.sdk.discovery_timeout_ms': 10000,
-	'ydb.sdk.discovery_interval_ms': 60000,
-}
+	'ydb.sdk.discovery_timeout_ms': 10_000,
+	'ydb.sdk.discovery_interval_ms': 60_000,
+} as const satisfies DriverOptions
 
 export class Driver implements Disposable {
 	protected readonly cs: URL
@@ -63,14 +65,16 @@ export class Driver implements Disposable {
 			throw new Error('Invalid connection string. Database name is required')
 		}
 
-		if (this.isSecure && !options.ssl) {
-			throw new Error('Secure connection requires SSL options')
-		}
-
 		if (this.cs.searchParams.has('application') === false) {
 			this.cs.searchParams.set('application', this.options['ydb.sdk.application'] || '')
 		} else {
 			this.options['ydb.sdk.application'] ??= this.cs.searchParams.get('application') || ''
+		}
+
+		const discoveryInterval = this.options['ydb.sdk.discovery_interval_ms'] ?? defaultOptions['ydb.sdk.discovery_interval_ms']!;
+		const discoveryTimeout = this.options['ydb.sdk.discovery_timeout_ms'] ?? defaultOptions['ydb.sdk.discovery_timeout_ms']!;
+		if (discoveryInterval < discoveryTimeout) {
+			throw new Error('Discovery interval must be greater than discovery timeout.')
 		}
 
 		let endpoint = create(EndpointInfoSchema, {
@@ -121,7 +125,7 @@ export class Driver implements Disposable {
 			// Periodic discovery
 			this.#rediscoverTimer = setInterval(() => {
 				this.#discovery(AbortSignal.timeout(this.options['ydb.sdk.discovery_timeout_ms']!))
-			}, this.options['ydb.sdk.discovery_interval_ms'] || 60 * 1000)
+			}, this.options['ydb.sdk.discovery_interval_ms'] || defaultOptions['ydb.sdk.discovery_interval_ms']!)
 
 			// Unref the timer so it doesn't keep the process running
 			this.#rediscoverTimer.unref()
@@ -140,19 +144,30 @@ export class Driver implements Disposable {
 	async #discovery(signal?: AbortSignal): Promise<void> {
 		dbg.extend("driver")("discovery(signal: %o)", signal)
 
-		let response = await this.#discoveryClient.listEndpoints({ database: this.database }, { signal })
-		if (!response.operation) {
-			throw new ClientError(DiscoveryServiceDefinition.listEndpoints.path, Status.UNKNOWN, 'No operation in response');
+		let retryConfig: RetryConfig = {
+			retry: (err) => err instanceof ClientError || err instanceof YDBError || err instanceof Error && err.name !== 'TimeoutError',
+			signal,
+			budget: Infinity,
+			strategy: exponential(50)
 		}
 
-		if (response.operation.status !== StatusIds_StatusCode.SUCCESS) {
-			throw new Error(`(${response.operation.status}) ${response.operation.issues}`)
-		}
+		let result = await retry(retryConfig, async () => {
+			let response = await this.#discoveryClient.listEndpoints({ database: this.database }, { signal })
+			if (!response.operation) {
+				throw new ClientError(DiscoveryServiceDefinition.listEndpoints.path, Status.UNKNOWN, 'No operation in response');
+			}
 
-		let result = anyUnpack(response.operation.result!, ListEndpointsResultSchema);
-		if (!result) {
-			throw new ClientError(DiscoveryServiceDefinition.listEndpoints.path, Status.UNKNOWN, 'No result in operation');
-		}
+			if (response.operation.status !== StatusIds_StatusCode.SUCCESS) {
+				throw new YDBError(response.operation.status, response.operation.issues)
+			}
+
+			let result = anyUnpack(response.operation.result!, ListEndpointsResultSchema);
+			if (!result) {
+				throw new ClientError(DiscoveryServiceDefinition.listEndpoints.path, Status.UNKNOWN, 'No result in operation');
+			}
+
+			return result
+		})
 
 		for (let endpoint of result.endpoints) {
 			this.#pool.add(endpoint)
@@ -163,7 +178,7 @@ export class Driver implements Disposable {
 		let abortPromise = new Promise<void>((_, reject) => {
 			if (signal) {
 				signal.addEventListener('abort', function abortHandler() {
-					reject(new Error('Operation aborted'))
+					reject(signal.reason)
 					signal.removeEventListener('abort', abortHandler)
 				})
 			}
