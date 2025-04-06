870b748daf3eeb2479e0a921466c2f772286f70d
Vladislav Polyakov <polrk@ydb.tech>
2025-04-06 06:51:13 +0000

    Add debug middleware for enhanced error handling in gRPC calls

---
+++ packages/core/src/middleware.ts
@@ -0,0 +1,25 @@
+import { ClientError, type ClientMiddleware } from "nice-grpc"
+
+import { dbg } from "./dbg.js"
+
+export const debug: ClientMiddleware = async function* (call, options) {
+	let hasError = false
+	try {
+		return yield* call.next(call.request, options)
+	} catch (error) {
+		hasError = true
+		if (error instanceof ClientError) {
+			dbg.extend('grpc')('%s', error.message)
+		} else if (error instanceof Error && error.name === 'AbortError') {
+			dbg.extend('grpc')('%s %s: %s', call.method.path, 'CANCELLED', error.message)
+		} else {
+			dbg.extend('grpc')('%s %s: %s', call.method.path, 'UNKNOWN', error)
+		}
+
+		throw error
+	} finally {
+		if (!hasError) {
+			dbg.extend('grpc')('%s %s', call.method.path, 'OK')
+		}
+	}
+}
