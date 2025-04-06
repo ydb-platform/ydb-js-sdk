867001b528479da8040cd86370eb6ba054ae3077
Vladislav Polyakov <polrk@ydb.tech>
2025-04-06 20:16:33 +0000

    Enhance devcontainer setup with SSH signing and YDB profile configuration

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -2,20 +2,21 @@
 
 source /usr/local/share/nvm/nvm.sh && nvm use --lts
 
+# Set up YDB profile if ydb cli exists
 if which ydb > /dev/null 2>&1; then
-    ENDPOINT=$(echo $YDB_CONNECTION_STRING | awk -F/ '{print $1 "//" $3}')
-    DATABASE=$(echo $YDB_CONNECTION_STRING | cut -d/ -f4-)
-    CA_FILE_OPTION=""
+	ENDPOINT=$(echo $YDB_CONNECTION_STRING | awk -F/ '{print $1 "//" $3}')
+	DATABASE=$(echo $YDB_CONNECTION_STRING | cut -d/ -f4-)
+	CA_FILE_OPTION=""
 
-    if [ -n "$YDB_SSL_ROOT_CERTIFICATES_FILE" ]; then
-        ENDPOINT="${ENDPOINT/grpc:/grpcs:}"
-        CA_FILE_OPTION="--ca-file ${YDB_SSL_ROOT_CERTIFICATES_FILE}"
-    fi
+	if [ -n "$YDB_SSL_ROOT_CERTIFICATES_FILE" ]; then
+		ENDPOINT="${ENDPOINT/grpc:/grpcs:}"
+		CA_FILE_OPTION="--ca-file ${YDB_SSL_ROOT_CERTIFICATES_FILE}"
+	fi
 
-    ydb config profile replace local \
-        --endpoint "$ENDPOINT" \
-        --database "/$DATABASE" \
-        $CA_FILE_OPTION
+	ydb config profile replace local \
+		--endpoint "$ENDPOINT" \
+		--database "/$DATABASE" \
+		$CA_FILE_OPTION
 
-    ydb config profile activate local
+	ydb config profile activate local
 fi
