9c195150c84ff87730721e7c43b60d78cffffbad
Vladislav Polyakov <polrk@ydb.tech>
2025-04-16 14:28:33 +0000

    Update YDB connection string handling to support secure variable fallback

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -4,8 +4,8 @@ source /usr/local/share/nvm/nvm.sh && nvm use --lts
 
 # Set up YDB profile if ydb cli exists
 if which ydb > /dev/null 2>&1; then
-	ENDPOINT=$(echo $YDB_CONNECTION_STRING | awk -F/ '{print $1 "//" $3}')
-	DATABASE=$(echo $YDB_CONNECTION_STRING | cut -d/ -f4-)
+	ENDPOINT=$(echo ${YDB_CONNECTION_STRING_SECURE:-YDB_CONNECTION_STRING} | awk -F/ '{print $1 "//" $3}')
+	DATABASE=$(echo ${YDB_CONNECTION_STRING_SECURE:-YDB_CONNECTION_STRING} | cut -d/ -f4-)
 	CA_FILE_OPTION=""
 
 	if [ -n "$YDB_SSL_ROOT_CERTIFICATES_FILE" ]; then
