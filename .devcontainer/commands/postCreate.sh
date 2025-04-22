c262a5f4e09a217e56ab31842c6e55a1b8dca6e0
Vladislav Polyakov <polrk@ydb.tech>
2025-04-22 11:46:23 +0000

    chore: streamline GPG signing configuration in postCreate.sh
    Signed-off-by: Vladislav Polyakov <polrk@ydb.tech>

--- .devcontainer/commands/postCreate.sh
+++ .devcontainer/commands/postCreate.sh
@@ -3,6 +3,12 @@ set -e
 
 source /usr/local/share/nvm/nvm.sh && nvm use --lts
 
+if git config --get commit.gpgsign | grep -q true; then
+	git config --global gpg.format ssh
+	git config --global gpg.ssh.defaultKeyCommand 'ssh-add -L'
+	git config --global gpg.ssh.allowedSigners '~/.ssh/allowed_signers'
+fi
+
 # Set up YDB profile if ydb cli exists
 if which ydb > /dev/null 2>&1; then
 	ENDPOINT=$(echo ${YDB_CONNECTION_STRING_SECURE:-$YDB_CONNECTION_STRING} | awk -F/ '{print $1 "//" $3}')
@@ -20,9 +26,4 @@ if which ydb > /dev/null 2>&1; then
 		$CA_FILE_OPTION
 
 	ydb config profile activate local
-fi
-
-if [ -f ~/.ssh/id_ed25519_signing ]; then
-	git config --global gpg.format ssh
-	git config --global commit.gpgsign true
 fi
