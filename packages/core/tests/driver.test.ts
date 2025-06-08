732126ab5faab98d185a71959c308df5d0d03273
Vladislav Polyakov <polRk@ydb.tech>
2025-06-08 14:29:25 +0300

    fix(tests): update driver test configurations and remove obsolete race condition test
    Signed-off-by: Vladislav Polyakov <polRk@ydb.tech>

--- packages/core/tests/driver.test.ts
+++ packages/core/tests/driver.test.ts
@@ -4,7 +4,7 @@ import { Driver } from '../dist/esm/driver.js'
 
 test('database in pathname', async () => {
 	let driver = new Driver('grpc://localhost:1234/path', {
-		'ydb.sdk.discovery_timeout_ms': 1000,
+		'ydb.sdk.enable_discovery': false,
 	})
 
 	expect(driver.database, 'Database is not set').toBe('/path')
@@ -12,7 +12,7 @@ test('database in pathname', async () => {
 
 test('database in querystring', async () => {
 	let driver = new Driver('grpc://localhost:1234?database=/query', {
-		'ydb.sdk.discovery_timeout_ms': 1000,
+		'ydb.sdk.enable_discovery': false,
 	})
 
 	expect(driver.database, 'Database is not set').toBe('/query')
