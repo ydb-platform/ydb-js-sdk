27e4f77455e06bd80b6f18fca4bf4d43a81d2f8a
Vladislav Polyakov <polRk@ydb.tech>
2025-07-11 12:08:27 +0300

    fix: update TX interface to extend Abortable and add transaction lifecycle methods
    Signed-off-by: Vladislav Polyakov <polRk@ydb.tech>

--- packages/topic/src/tx.ts
+++ packages/topic/src/tx.ts
@@ -1,5 +1,9 @@
-export type TX = {
+import type { Abortable } from "node:events"
+
+export interface TX extends Abortable {
 	sessionId: string
 	transactionId: string
-	registerPrecommitHook: (fn: () => Promise<void> | void) => void
+	onRollback: (fn: (error: unknown, signal?: AbortSignal) => Promise<void> | void) => void
+	onCommit: (fn: (signal?: AbortSignal) => Promise<void> | void) => void
+	onClose: (fn: (committed: boolean, signal?: AbortSignal) => Promise<void> | void) => void
 }
